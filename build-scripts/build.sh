ff2d9f93829ceab104aec58d1cd976a41c6203f0
The-city-not-present <23407369+The-city-not-present@users.noreply.github.com>
2026-05-23 18:51:44 +0300

    upd build system

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -28,18 +28,6 @@ echo "done"
 echo -
 echo -
 
-echo "Re-build htmler..."
-pushd src/endpoints/lib/htmltmpl
-if [ -f compiled/html_bundle.py ]; then
-  rm -rf compiled/html_bundle.py
-fi
-make init
-make build-only-static
-popd
-echo "done"
-echo -
-echo -
-
 echo "Produce \"webserve_bundle.py\""
 echo "Calling pinliner..."
 # if [ ! -f "src-make/lib/pinliner/pinliner/pinliner.py" ]; then
@@ -58,14 +46,6 @@ echo "# print('within webserve_bundle')" >> "dist/webserve_bundle.py"
 echo "from src import launcher" >> "dist/webserve_bundle.py"
 echo "launcher.main()" >> "dist/webserve_bundle.py"
 echo "# print('out of webserve_bundle')" >> "dist/webserve_bundle.py"
-echo "done"
-echo -
-echo -
-
-
-echo "Bring \"static\" files to ./static/"
-mkdir -p ./dist/static
-rsync -av --exclude='*.py' src/endpoints/lib/htmltmpl/compiled/ ./dist/static/
 echo "done"
 echo -
 echo -
